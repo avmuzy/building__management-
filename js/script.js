39498d286b80b4f5d82083bd10caf3a5e3abcd23
avms <andrevmuzy@gmail.com>
2022-08-19 18:56:58 -0300

    calculate func

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-console.log('test');
+function start() {
+  console.log('start');
+  console.log(calculateTotalApartments(10, 10, 10));
+}
+
+function calculateTotalApartments(towers, floors, apartmentsByFloor) {
+  return towers * floors * apartmentsByFloor;
+}
+
+start();
