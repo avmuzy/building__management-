fc72a8a4fe001153bf6e18e0ae58c12e21cfda6d
avms <andrevmuzy@gmail.com>
2022-08-30 11:00:59 -0300

    month revenue

--- js/script_building.js
+++ js/script_building.js
@@ -13,9 +13,11 @@ function calculateTotalApartments() {
 function calculateTax() {
   var revn1 = window.document.getElementById('monthRevenue');
   var revn2 = window.document.getElementById('taxes');
-  var resrev = window.document.getElementById(revRes);
+  var resrev = window.document.getElementById('revRes');
   var rev1 = Number(revn1.value);
   var rev2 = Number(revn2.value);
   var revtot = (rev1 + rev2) * 12;
+  var revmonth = rev1 + rev2;
+  monRev.innerHTML = `Monthly revnue : ${revmonth}`;
   revRes.innerHTML = ` Annual revenue: ${revtot}`;
 }
